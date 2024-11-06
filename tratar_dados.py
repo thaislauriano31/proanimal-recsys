c30f7d1284689fbc88e685b0ee5930d359d2f943
GusSampaio <gustavo.sampaio@usp.br>
2024-11-06 20:28:57 -0300

    Base tratada

---
+++ tratar_dados.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+path = 'dataset/dogs_data.csv'
+
+# Lendo o arquivo
+df = pd.read_csv(path)
+
+# Tratando valores de idade
+df['IDADE'] = df['IDADE'].str.replace(' anos', '')
+df['IDADE'] = df['IDADE'].str.strip()
+
+df['PORTE'] = df['PORTE'].str.strip()
+
+# Se porte pequeno então substituir por 1, se medio por 2 e se grande por 3
+df['PORTE'] = df['PORTE'].apply(lambda x: 1 if x.lower() == 'pequeno' else 2 if x.lower() == 'médio' else 3)
+
+# Tratando peso
+df['VACINADO'] = df['VACINADO'].apply(lambda x: 1 if x.lower() == 'sim' else 0)
+
+df['CASTRADO'] = df['CASTRADO'].apply(lambda x: 1 if x.lower() == 'sim' else 0)
+
+df['Se dá bem com outros animais'] = df['Se dá bem com outros animais'].apply(lambda x: 1 if x.lower() == 'não, precisa ser filho único' else 2 if x.lower() == 'só cães' else 3)
+
+df['Adoção especial?'] = df['Adoção especial?'].apply(lambda x: 1 if x.lower() == 'sim' else 0)
+
+df['Energia'] = df['Energia'].apply(lambda x: 1 if x.lower() == 'baixa' else 2 if x.lower() == 'média' else 3)
+
+# Salvando o arquivo
+df.to_csv('dataset/dogs_data_tratado.csv', index=False)
